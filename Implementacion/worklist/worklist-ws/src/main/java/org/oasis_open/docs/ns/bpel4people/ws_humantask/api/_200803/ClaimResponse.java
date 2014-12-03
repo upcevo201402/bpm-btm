19d951dc3012d63534cab0f95ace44318a1b1539
jayvar <jair_1201@hotmail.com>
2014-12-02 22:13:51 -0500

    Signed-off-by: jayvar <jair_1201@hotmail.com>

--- Implementacion/worklist/worklist-ws/src/main/java/org/oasis_open/docs/ns/bpel4people/ws_humantask/api/_200803/ClaimResponse.java
+++ Implementacion/worklist/worklist-ws/src/main/java/org/oasis_open/docs/ns/bpel4people/ws_humantask/api/_200803/ClaimResponse.java
@@ -5,6 +5,7 @@ import javax.xml.bind.annotation.XmlAccessType;
 import javax.xml.bind.annotation.XmlAccessorType;
 import javax.xml.bind.annotation.XmlRootElement;
 import javax.xml.bind.annotation.XmlType;
+import java.applet.Applet;
 
 
 /**
